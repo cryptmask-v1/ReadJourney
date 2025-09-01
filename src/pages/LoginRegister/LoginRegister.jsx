0bcdd61719ac44b1952ca68a16bf2f259d26d297
cryptmask <emreozkurt1978@gmail.com>
2025-09-01 04:36:37 +0300

    feat: add modern-normalize for consistent styling and enhance Login/Register page with validation and improved layout

--- src/pages/LoginRegister/LoginRegister.jsx
+++ src/pages/LoginRegister/LoginRegister.jsx
@@ -1,10 +1,13 @@
 import styles from "./LoginRegister.module.css";
-import { Formik } from "formik";
+import { ErrorMessage, Formik } from "formik";
 import { loginUser, registerUser } from "../../store/Auth/authService.js";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
+import * as yup from "yup";
+import logo from "../../assets/logo.png";
+import hero from "../../assets/loginregister.png";
 
 const LoginRegister = () => {
   const dispatch = useDispatch();
@@ -17,6 +20,29 @@ const LoginRegister = () => {
       navigate("/recommended");
     }
   }, [token, navigate]);
+
+  const loginSchema = yup.object().shape({
+    email: yup
+      .string()
+      .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(7, "Password must be at least 7 characters")
+      .required("Password is required"),
+  });
+
+  const registerSchema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .min(7, "Password must be at least 7 characters")
+      .required("Password is required"),
+  });
 
   const notify = (message, type) => {
     if (!message) return;
@@ -63,85 +89,115 @@ const LoginRegister = () => {
       <div className={styles.sections}>
         <>
           <div className={styles.leftSection}>
-            {location.pathname.includes("/register") && (
-              <Formik
-                key={location.pathname}
-                initialValues={{ name: "", email: "", password: "" }}
-                onSubmit={handleRegister}
-              >
-                {({ values, handleChange, handleSubmit, isSubmitting }) => (
-                  <form onSubmit={handleSubmit}>
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="Name"
-                      onChange={handleChange}
-                      value={values.name}
-                    />
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      onChange={handleChange}
-                      value={values.email}
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      onChange={handleChange}
-                      value={values.password}
-                    />
-                    <button type="submit" disabled={isSubmitting}>
-                      Submit
-                    </button>
-                    <Link
-                      style={{ textDecoration: "none", color: "white" }}
-                      to="/login"
+            <div className={styles.logoContainer}>
+              <img src={logo} alt="Logo" />
+            </div>
+            <h2 className={styles.registerLoginTitle}>
+              Expend your mind, reading <span>a book</span>
+            </h2>
+            <div className={styles.formContainer}>
+              {location.pathname.includes("/register") && (
+                <Formik
+                  key={location.pathname}
+                  initialValues={{ name: "", email: "", password: "" }}
+                  validationSchema={registerSchema}
+                  onSubmit={handleRegister}
+                >
+                  {({ values, handleChange, handleSubmit, isSubmitting }) => (
+                    <form
+                      className={styles.formSection}
+                      onSubmit={handleSubmit}
                     >
-                      Already have an account?
-                    </Link>
-                  </form>
-                )}
-              </Formik>
-            )}
-            {location.pathname.includes("/login") && (
-              <Formik
-                key={location.pathname}
-                initialValues={{ email: "", password: "" }}
-                onSubmit={handleLogin}
-              >
-                {({ values, handleChange, handleSubmit, isSubmitting }) => (
-                  <form onSubmit={handleSubmit}>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Email"
-                      onChange={handleChange}
-                      value={values.email}
-                    />
-                    <input
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                      onChange={handleChange}
-                      value={values.password}
-                    />
-                    <button type="submit" disabled={isSubmitting}>
-                      Submit
-                    </button>
-                    <Link
-                      style={{ textDecoration: "none", color: "white" }}
-                      to="/register"
+                      <input
+                        type="text"
+                        name="name"
+                        placeholder="Name"
+                        onChange={handleChange}
+                        value={values.name}
+                      />
+                      <ErrorMessage name="name" component="div" />
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={handleChange}
+                        value={values.email}
+                      />
+                      <ErrorMessage name="email" component="div" />
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        value={values.password}
+                      />
+                      <ErrorMessage name="password" component="div" />
+                      <div className={styles.btnSection}>
+                        <button
+                          className={styles.registerBtn}
+                          type="submit"
+                          disabled={isSubmitting}
+                        >
+                          Register
+                        </button>
+                        <Link className={styles.btnSectionLink} to="/login">
+                          Already have an account?
+                        </Link>
+                      </div>
+                    </form>
+                  )}
+                </Formik>
+              )}
+              {location.pathname.includes("/login") && (
+                <Formik
+                  key={location.pathname}
+                  initialValues={{ email: "", password: "" }}
+                  onSubmit={handleLogin}
+                  validationSchema={loginSchema}
+                >
+                  {({ values, handleChange, handleSubmit, isSubmitting }) => (
+                    <form
+                      className={styles.formSection}
+                      onSubmit={handleSubmit}
                     >
-                      Don't have an account?
-                    </Link>
-                  </form>
-                )}
-              </Formik>
-            )}
+                      <ErrorMessage name="email" component="div" />
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        onChange={handleChange}
+                        value={values.email}
+                      />
+                      <ErrorMessage name="password" component="div" />
+                      <input
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        onChange={handleChange}
+                        value={values.password}
+                      />
+                      <button
+                        className={styles.loginBtn}
+                        type="submit"
+                        disabled={isSubmitting}
+                      >
+                        Login
+                      </button>
+                      <Link
+                        style={{ textDecoration: "none", color: "white" }}
+                        to="/register"
+                      >
+                        Don't have an account?
+                      </Link>
+                    </form>
+                  )}
+                </Formik>
+              )}
+            </div>
           </div>
-          <div className={styles.rightSection}></div>
+          <div className={styles.rightSection}>
+            <img src={hero} alt="Hero" />
+          </div>
         </>
       </div>
     </div>
